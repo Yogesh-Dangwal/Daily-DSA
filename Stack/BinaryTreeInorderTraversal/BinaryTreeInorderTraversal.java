d353b7f679365f1740e3e93f956b99d9e982359c
Yogesh Dangwal <yogeshdangwal123@gmail.com>
2026-01-05 20:22:08 +0530

    Day 6

---
+++ Stack/BinaryTreeInorderTraversal/BinaryTreeInorderTraversal.java
@@ -0,0 +1,5 @@
+package Stack.BinaryTreeInorderTraversal;
+
+public class BinaryTreeInorderTraversal {
+    
+}
---
+++ Stack/BinaryTreePreorderTraversal/BinaryTreePreorderTraversal.java
@@ -0,0 +1,37 @@
+package Stack.BinaryTreePreorderTraversal;
+import java.util.*;
+
+public class BinaryTreePreorderTraversal {
+
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+    }
+
+    public static List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        preorder(root, result);
+        return result;
+    }
+
+    private static void preorder(TreeNode node, List<Integer> result) {
+        if (node == null) return;
+
+        result.add(node.val);
+        preorder(node.left, result);
+        preorder(node.right, result);
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(1);
+        root.right = new TreeNode(2);
+        root.right.left = new TreeNode(3);
+
+        System.out.println(preorderTraversal(root));
+    }
+}
