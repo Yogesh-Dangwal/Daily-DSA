f52057991d8c08643dceb7854866d0ff8eaba385
Yogesh Dangwal <yogeshdangwal123@gmail.com>
2026-01-05 20:13:28 +0530

    Day 6

---
+++ Sorting/SortList/SortList.java
@@ -0,0 +1,71 @@
+package Sorting.SortList;
+public class SortList {
+
+    static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int val) {
+            this.val = val;
+        }
+    }
+
+    public static ListNode sortList(ListNode head) {
+        if (head == null || head.next == null) return head;
+
+        ListNode mid = getMid(head);
+        ListNode right = mid.next;
+        mid.next = null;
+
+        ListNode leftSorted = sortList(head);
+        ListNode rightSorted = sortList(right);
+
+        return merge(leftSorted, rightSorted);
+    }
+
+    private static ListNode getMid(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head.next;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        return slow;
+    }
+
+    private static ListNode merge(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(0);
+        ListNode curr = dummy;
+
+        while (l1 != null && l2 != null) {
+            if (l1.val <= l2.val) {
+                curr.next = l1;
+                l1 = l1.next;
+            } else {
+                curr.next = l2;
+                l2 = l2.next;
+            }
+            curr = curr.next;
+        }
+
+        curr.next = (l1 != null) ? l1 : l2;
+        return dummy.next;
+    }
+
+    public static void main(String[] args) {
+        ListNode head = new ListNode(4);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(1);
+        head.next.next.next = new ListNode(3);
+
+        ListNode sorted = sortList(head);
+
+        while (sorted != null) {
+            System.out.print(sorted.val);
+            if (sorted.next != null) System.out.print(" -> ");
+            sorted = sorted.next;
+        }
+    }
+}
