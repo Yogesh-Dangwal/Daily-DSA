586b9ee68011e93e6167ce4863609295d6ac6dfa
Yogesh Dangwal <yogeshdangwal123@gmail.com>
2026-01-12 22:54:25 +0530

    Day 13

--- Array/MinimumTimeVisitingAllPoints/MinimumTimeVisitingAllPoints.java
+++ Array/MinimumTimeVisitingAllPoints/MinimumTimeVisitingAllPoints.java
@@ -1,5 +1,25 @@
 package Array.MinimumTimeVisitingAllPoints;
-
 public class MinimumTimeVisitingAllPoints {
-    
+
+    public static int minTimeToVisitAllPoints(int[][] points) {
+        int time = 0;
+
+        for (int i = 1; i < points.length; i++) {
+            int dx = Math.abs(points[i][0] - points[i - 1][0]);
+            int dy = Math.abs(points[i][1] - points[i - 1][1]);
+
+            time += Math.max(dx, dy);
+        }
+
+        return time;
+    }
+
+    public static void main(String[] args) {
+
+        int[][] points1 = {{1, 1}, {3, 4}, {-1, 0}};
+        System.out.println(minTimeToVisitAllPoints(points1));
+
+        int[][] points2 = {{3, 2}, {-2, 2}};
+        System.out.println(minTimeToVisitAllPoints(points2));
+    }
 }
