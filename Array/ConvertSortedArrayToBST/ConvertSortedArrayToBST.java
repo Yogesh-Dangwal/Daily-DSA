4e7b3fc45081724b32e078c3976d46bba4810afb
Yogesh Dangwal <yogeshdangwal123@gmail.com>
2026-01-12 23:00:11 +0530

    Day 13

--- Array/ConvertSortedArrayToBST/ConvertSortedArrayToBST.java
+++ Array/ConvertSortedArrayToBST/ConvertSortedArrayToBST.java
@@ -1,5 +1,42 @@
 package Array.ConvertSortedArrayToBST;
-
 public class ConvertSortedArrayToBST {
-    
+
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+    }
+
+    public static TreeNode sortedArrayToBST(int[] nums) {
+        return build(nums, 0, nums.length - 1);
+    }
+
+    private static TreeNode build(int[] nums, int left, int right) {
+        if (left > right) return null;
+
+        int mid = (left + right) / 2;
+        TreeNode root = new TreeNode(nums[mid]);
+
+        root.left = build(nums, left, mid - 1);
+        root.right = build(nums, mid + 1, right);
+
+        return root;
+    }
+    private static void inorder(TreeNode root) {
+        if (root == null) return;
+        inorder(root.left);
+        System.out.print(root.val + " ");
+        inorder(root.right);
+    }
+
+    public static void main(String[] args) {
+
+        int[] nums = {-10, -3, 0, 5, 9};
+        TreeNode root = sortedArrayToBST(nums);
+        inorder(root);
+    }
 }
