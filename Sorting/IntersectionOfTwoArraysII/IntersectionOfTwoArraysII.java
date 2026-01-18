2fb62878ef0264bdbedf2a1cca7436234b1205b0
Yogesh Dangwal <yogeshdangwal123@gmail.com>
2026-01-18 20:16:43 +0530

    Day 19

---
+++ Sorting/IntersectionOfTwoArraysII/IntersectionOfTwoArraysII.java
@@ -0,0 +1,38 @@
+package Sorting.IntersectionOfTwoArraysII;
+import java.util.*;
+
+public class IntersectionOfTwoArraysII {
+
+    public static int[] intersect(int[] nums1, int[] nums2) {
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int num : nums1) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+
+        List<Integer> list = new ArrayList<>();
+
+        for (int num : nums2) {
+            if (map.containsKey(num) && map.get(num) > 0) {
+                list.add(num);
+                map.put(num, map.get(num) - 1);
+            }
+        }
+
+        int[] result = new int[list.size()];
+        for (int i = 0; i < list.size(); i++) {
+            result[i] = list.get(i);
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int[] nums1 = {1,2,2,1};
+        int[] nums2 = {2,2};
+
+        int[] res = intersect(nums1, nums2);
+        System.out.println(Arrays.toString(res));
+    }
+}
