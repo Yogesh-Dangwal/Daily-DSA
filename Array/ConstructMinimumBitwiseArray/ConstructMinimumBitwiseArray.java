a3202b9703597aca25d06c479b26373fe1dd8932
Yogesh Dangwal <yogeshdangwal123@gmail.com>
2026-01-20 22:36:05 +0530

    Day 21

--- Array/ConstructMinimumBitwiseArray/ConstructMinimumBitwiseArray.java
+++ Array/ConstructMinimumBitwiseArray/ConstructMinimumBitwiseArray.java
@@ -1,5 +1,33 @@
 package Array.ConstructMinimumBitwiseArray;
+import java.util.*;
 
 public class ConstructMinimumBitwiseArray {
-    
+
+    public static int[] minBitwiseArray(int[] nums) {
+        int n = nums.length;
+        int[] ans = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            int num = nums[i];
+
+            if ((num & (num - 1)) == 0) {
+                ans[i] = -1;
+                continue;
+            }
+
+            int bit = 0;
+            while (((num >> bit) & 1) == 1) {
+                bit++;
+            }
+
+            ans[i] = num ^ (1 << bit);
+        }
+
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {2, 3, 5, 7};
+        System.out.println(Arrays.toString(minBitwiseArray(nums)));
+    }
 }
