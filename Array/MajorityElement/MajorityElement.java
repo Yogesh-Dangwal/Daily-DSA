cb79bf900907940eb2a2ddad90a1e253d72ffbf4
Yogesh Dangwal <yogeshdangwal123@gmail.com>
2026-01-23 23:29:14 +0530

    Day 24

--- Array/MajorityElement/MajorityElement.java
+++ Array/MajorityElement/MajorityElement.java
@@ -1,5 +1,26 @@
 package Array.MajorityElement;
-
 public class MajorityElement {
-    
+
+    public static int majorityElement(int[] nums) {
+        int freq = 0, res = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (freq == 0) {
+                res = nums[i];
+            }
+
+            if (nums[i] == res) {
+                freq++;
+            } else {
+                freq--;
+            }
+        }
+
+        return res;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {2, 2, 1, 1, 1, 2, 2};
+        System.out.println(majorityElement(nums)); // 2
+    }
 }
