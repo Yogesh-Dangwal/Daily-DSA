758d6242b0bc48bcb4574b13b75e55e0f804a4ab
Yogesh Dangwal <yogeshdangwal123@gmail.com>
2026-01-24 23:25:37 +0530

    Day 25

--- Array/MinimizeMaximumPairSum/MinimizeMaximumPairSum.java
+++ Array/MinimizeMaximumPairSum/MinimizeMaximumPairSum.java
@@ -1,5 +1,26 @@
 package Array.MinimizeMaximumPairSum;
+import java.util.*;
 
 public class MinimizeMaximumPairSum {
-    
+
+    public static int minPairSum(int[] nums) {
+        Arrays.sort(nums);
+
+        int left = 0;
+        int right = nums.length - 1;
+        int ans = 0;
+
+        while (left < right) {
+            ans = Math.max(ans, nums[left] + nums[right]);
+            left++;
+            right--;
+        }
+
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {3, 5, 2, 3};
+        System.out.println(minPairSum(nums)); // 7
+    }
 }
