75417e42cfa76a06a14b3a625eca8d5bde570123
Yogesh Dangwal <yogeshdangwal123@gmail.com>
2026-01-26 23:01:05 +0530

    Day 27

---
+++ Array/MinimumAbsoluteDifference/MinimumAbsoluteDifference.java
@@ -0,0 +1,29 @@
+package Array.MinimumAbsoluteDifference;
+import java.util.*;
+
+public class MinimumAbsoluteDifference {
+
+    public static List<List<Integer>> minimumAbsDifference(int[] arr) {
+        Arrays.sort(arr);
+        List<List<Integer>> result = new ArrayList<>();
+
+        int minDiff = Integer.MAX_VALUE;
+
+        for (int i = 1; i < arr.length; i++) {
+            minDiff = Math.min(minDiff, arr[i] - arr[i - 1]);
+        }
+
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] - arr[i - 1] == minDiff) {
+                result.add(Arrays.asList(arr[i - 1], arr[i]));
+            }
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {4, 2, 1, 3};
+        System.out.println(minimumAbsDifference(arr));
+    }
+}
