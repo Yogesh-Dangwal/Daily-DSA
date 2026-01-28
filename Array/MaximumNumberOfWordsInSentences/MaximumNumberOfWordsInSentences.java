195bfd7d56cd2140bd21ec06b877207975acf1c4
Yogesh Dangwal <yogeshdangwal123@gmail.com>
2026-01-28 21:04:03 +0530

    Day 29

--- Array/MaximumNumberOfWordsInSentences/MaximumNumberOfWordsInSentences.java
+++ Array/MaximumNumberOfWordsInSentences/MaximumNumberOfWordsInSentences.java
@@ -1,5 +1,27 @@
 package Array.MaximumNumberOfWordsInSentences;
-
 public class MaximumNumberOfWordsInSentences {
-    
+
+    public static int mostWordsFound(String[] sentences) {
+        int maxWords = 0;
+
+        for (String s : sentences) {
+            int count = 1;
+            for (char c : s.toCharArray()) {
+                if (c == ' ') count++;
+            }
+            maxWords = Math.max(maxWords, count);
+        }
+
+        return maxWords;
+    }
+
+    public static void main(String[] args) {
+        String[] sentences = {
+            "alice and bob love leetcode",
+            "i think so too",
+            "this is great thanks very much"
+        };
+
+        System.out.println(mostWordsFound(sentences));
+    }
 }
