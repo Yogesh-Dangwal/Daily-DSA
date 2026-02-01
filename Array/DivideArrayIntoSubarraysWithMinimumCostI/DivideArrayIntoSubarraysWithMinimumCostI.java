ddcbd65d189651d59b57c093343ddbfd5e4be75e
Yogesh Dangwal <yogeshdangwal123@gmail.com>
2026-02-01 23:53:03 +0530

    Day 33

--- Array/DivideArrayIntoSubarraysWithMinimumCostI/DivideArrayIntoSubarraysWithMinimumCostI.java
+++ Array/DivideArrayIntoSubarraysWithMinimumCostI/DivideArrayIntoSubarraysWithMinimumCostI.java
@@ -1,5 +1,23 @@
 package Array.DivideArrayIntoSubarraysWithMinimumCostI;
-
 public class DivideArrayIntoSubarraysWithMinimumCostI {
-    
+
+    public static int minimumCost(int[] nums) {
+        int min1 = Integer.MAX_VALUE, min2 = Integer.MAX_VALUE;
+
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] < min1) {
+                min2 = min1;
+                min1 = nums[i];
+            } else if (nums[i] < min2) {
+                min2 = nums[i];
+            }
+        }
+
+        return nums[0] + min1 + min2;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {10, 3, 1, 1};
+        System.out.println(minimumCost(nums));
+    }
 }
