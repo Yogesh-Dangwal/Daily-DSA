bebaeb3a30bae95ff004dff5b0a5e13d9db8095d
Yogesh Dangwal <yogeshdangwal123@gmail.com>
2026-02-02 21:24:37 +0530

    Day 34

--- Stack/MyQueueUsingStacks/MyQueueUsingStacks.java
+++ Stack/MyQueueUsingStacks/MyQueueUsingStacks.java
@@ -1,5 +1,51 @@
 package Stack.MyQueueUsingStacks;
+import java.util.*;
+
+class MyQueue {
+
+    private Stack<Integer> inStack;
+    private Stack<Integer> outStack;
+
+    public MyQueue() {
+        inStack = new Stack<>();
+        outStack = new Stack<>();
+    }
+    
+    public void push(int x) {
+        inStack.push(x);
+    }
+    
+    private void moveStacks() {
+        if (outStack.isEmpty()) {
+            while (!inStack.isEmpty()) {
+                outStack.push(inStack.pop());
+            }
+        }
+    }
+    
+    public int pop() {
+        moveStacks();
+        return outStack.pop();
+    }
+    
+    public int peek() {
+        moveStacks();
+        return outStack.peek();
+    }
+    
+    public boolean empty() {
+        return inStack.isEmpty() && outStack.isEmpty();
+    }
+}
 
 public class MyQueueUsingStacks {
-    
+    public static void main(String[] args) {
+        MyQueue queue = new MyQueue();
+
+        queue.push(1);
+        queue.push(2);
+        System.out.println(queue.peek());
+        System.out.println(queue.pop());
+        System.out.println(queue.empty());
+    }
 }
