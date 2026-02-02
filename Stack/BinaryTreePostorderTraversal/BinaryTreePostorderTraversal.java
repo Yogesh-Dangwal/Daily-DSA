3a1cddf4c00eedeb08cace3cb04d2a60140bd078
Yogesh Dangwal <yogeshdangwal123@gmail.com>
2026-02-02 20:56:59 +0530

    Day 34

---
+++ Stack/BinaryTreePostorderTraversal/BinaryTreePostorderTraversal.java
@@ -0,0 +1,36 @@
+package Stack.BinaryTreePostorderTraversal;
+import java.util.*;
+
+class TreeNode {
+    int val;
+    TreeNode left, right;
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+}
+
+public class BinaryTreePostorderTraversal {
+
+    public static List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        traverse(root, result);
+        return result;
+    }
+
+    private static void traverse(TreeNode node, List<Integer> result) {
+        if (node == null) return;
+
+        traverse(node.left, result);
+        traverse(node.right, result);
+        result.add(node.val);
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(1);
+        root.right = new TreeNode(2);
+        root.right.left = new TreeNode(3);
+
+        System.out.println(postorderTraversal(root));
+    }
+}
