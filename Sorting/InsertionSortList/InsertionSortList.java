f7f5fe0ccd06120edb4f5fd00cbf8a39f8151d5b
Yogesh Dangwal <yogeshdangwal123@gmail.com>
2026-03-26 23:45:28 +0530

    Day 87

--- Sorting/InsertionSortList/InsertionSortList.java
+++ Sorting/InsertionSortList/InsertionSortList.java
@@ -1,48 +1,55 @@
 package Sorting.InsertionSortList;
+class ListNode{
+
+    int val;
+    ListNode next;
+
+    ListNode(int v){
+        val=v;
+    }
+}
+
 public class InsertionSortList {
 
-    static class ListNode {
-        int val;
-        ListNode next;
+    public static ListNode insertionSortList(ListNode head){
 
-        ListNode(int val) {
-            this.val = val;
-        }
-    }
+        ListNode dummy=new ListNode(0);
 
-    public static ListNode insertionSortList(ListNode head) {
-        ListNode dummy = new ListNode(0);
-        ListNode curr = head;
+        while(head!=null){
 
-        while (curr != null) {
-            ListNode next = curr.next;
-            ListNode prev = dummy;
+            ListNode next=head.next;
 
-            while (prev.next != null && prev.next.val < curr.val) {
-                prev = prev.next;
-            }
+            ListNode prev=dummy;
 
-            curr.next = prev.next;
-            prev.next = curr;
-            curr = next;
+            while(prev.next!=null && prev.next.val<head.val)
+                prev=prev.next;
+
+            head.next=prev.next;
+            prev.next=head;
+
+            head=next;
         }
 
         return dummy.next;
     }
 
-    public static void main(String[] args) {
+    public static void print(ListNode head){
 
-        ListNode head = new ListNode(4);
-        head.next = new ListNode(2);
-        head.next.next = new ListNode(1);
-        head.next.next.next = new ListNode(3);
-
-        ListNode sorted = insertionSortList(head);
-
-        while (sorted != null) {
-            System.out.print(sorted.val);
-            if (sorted.next != null) System.out.print(" -> ");
-            sorted = sorted.next;
+        while(head!=null){
+            System.out.print(head.val+" ");
+            head=head.next;
         }
+    }
+
+    public static void main(String[] args){
+
+        ListNode head=new ListNode(4);
+        head.next=new ListNode(2);
+        head.next.next=new ListNode(1);
+        head.next.next.next=new ListNode(3);
+
+        head=insertionSortList(head);
+
+        print(head);
     }
 }
